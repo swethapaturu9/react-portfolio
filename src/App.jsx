30355187bc8bb07fabcb1413b232bff5e345aa8a
swethapaturu9 <114616110+swethapaturu9@users.noreply.github.com>
2023-08-11 20:04:32 -0400

    all folders created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+
+const App = () => {
+  return (
+    <div>
+        <Header/>
+        <Nav/>
+        
+    </div>
+  ) 
+}
+
+export default App
