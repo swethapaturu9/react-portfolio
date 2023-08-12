d1694fa26473aecf722ab02ca35a7c616e2544bb
swethapaturu9 <114616110+swethapaturu9@users.noreply.github.com>
2023-08-11 20:49:09 -0400

    styles and css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Header from './components/header/Header.jsx'
 import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+import Experience from './components/experience/Experience.jsx'
+import Projects from './components/projects/Projects.jsx'
+import Contact from './components/contact/Contact.jsx'
 
 const App = () => {
   return (
     <div>
         <Header/>
         <Nav/>
-        
+        <About/>
+        <Experience/>
+        <Projects/>
+        <Contact/>
     </div>
   ) 
 }
