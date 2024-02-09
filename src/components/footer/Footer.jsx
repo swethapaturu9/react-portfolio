ba9f40016f9754b8ecd206e699282abd1a52bbca
Swetha Paturu <paturu.s@northeastern.edu>
2024-02-09 16:08:11 -0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React,  { useEffect, useState } from 'react'
 import './footer.css'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
 import {SiLeetcode} from 'react-icons/si'
 
 const Footer = () => {
+
+  const [views, setViews] = useState('Loading...');
+
+  useEffect(() => {
+  
+    const lambdaURL = 'https://eg2yyqmmhms6mptmer2op4bdwa0xzjqv.lambda-url.us-east-1.on.aws/';
+
+    fetch(lambdaURL)
+      .then(response => response.json())
+      .then(data => {
+        setViews(data.newVisitorCount);
+      })
+      .catch(error => {
+        console.error('Error fetching view count:', error);
+        setViews('Error fetching views');
+      });
+  }, []);
+  
   return (
     <footer>
        <ul className="links"> 
@@ -19,6 +37,11 @@ const Footer = () => {
         <a href="https://github.com/swethapaturu9" target="_blank" rel="noreferrer noopener"><BsGithub/></a>
         <a href="https://leetcode.com/swetha_paturu/" target="_blank" rel="noreferrer noopener"><SiLeetcode/></a>  
     </div>
+
+    <div className="footer__views">
+      <small> Views: {views}</small>
+    </div>
+      
      <div className="footer__copyright">
      <small> &copy; Swetha Paturu </small>
 
