90975f71ae7160f7512a35e5004e028dec45d706
Swetha Paturu <paturu.s@northeastern.edu>
2024-02-09 20:48:36 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -62,7 +62,7 @@ resource "aws_lambda_function" "visitor_counter" {
 }
 
 
-resource "aws_lambda_function_url" "lambda-url" {
+resource "aws_lambda_function_url" "url" {
   function_name      = "visitorCounter"
   authorization_type = "NONE"
 
